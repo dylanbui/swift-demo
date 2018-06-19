c7bf76726c669c386c98d9be973e408528311dd9
Dylan Bui <buivantienduc@yahoo.com>
2018-06-19 18:56:12 +0700

    commit

--- SwiftApp/DbLibraries/UtilsClass/DbNotification.swift
+++ SwiftApp/DbLibraries/UtilsClass/DbNotification.swift
@@ -33,20 +33,182 @@ extension DbNotification {
     }
 }
 
-@objc protocol DbEventProtocol {
+//@objc protocol DbEventProtocol {
+//
+//    func startEvent(_ notification: DbNotification) -> Void
+//    func cancelEvent() -> Void;
+//    func eventRunBackgroundMode() -> [String]
+//
+//    @objc optional func eventPriority() -> Int
+//    @objc optional func eventGroup() -> String
+//}
+
+// -- Protocol for AnyObject --
+protocol DbEventProtocol : AnyObject {
+    
+    var eventID: DbEventRegisterID { get set }      //read-write
     
     func startEvent(_ notification: DbNotification) -> Void
     func cancelEvent() -> Void;
     func eventRunBackgroundMode() -> [String]
     
-    @objc optional func eventPriority() -> Int
-    @objc optional func eventGroup() -> String
+    func eventPriority() -> Int
+    func eventGroup() -> String
+
+    
 }
 
-//let numberSequence: SequenceType<Int>
+// -- Define for optional --
+extension DbEventProtocol {
+    
+    //var eventID: DbEventRegisterID = 0
+    
+//    var eventID: DbEventRegisterID {
+//        get {
+//            return 0
+//        }
+//        set(newValue) {
+////            let tmpAddress = String(format: "%p", unsafeBitCast(self, to: Int.self))
+////            UIViewController._myComputedProperty[tmpAddress] = newValue
+//            //self.eventID = newValue
+//            eventID = newValue
+//        }
+//    }
+    
+    
+    
+    func eventPriority() -> Int {
+        return 1;
+    }
+    
+    func eventGroup() -> String {
+        return "USER"
+    }
+}
 
 //typealias DbEventObject = Array<DbEventProtocol> // Dung
 typealias DbEventObject = DbEventProtocol // Dung
 
 //Since Swift 3:
 //typealias CellDelegate = UIPickerViewDataSource & UIPickerViewDelegate
+
+class DbEventIDGenerator {
+ 
+    static var  _nextEventRegisterID: DbEventRegisterID = 0
+    
+    class func getUniqueRegisterID() -> DbEventRegisterID {
+        self._nextEventRegisterID += 1;
+        return self._nextEventRegisterID;
+    }
+}
+
+class DbEventNotification {
+    
+    // MARK: - Properties
+    static let shared = DbEventNotification()
+    
+    private var arrEventRegisted: [DbEventObject] = []
+    private var arrSupportMode: [String] = []
+    
+    // Private Initialization
+    private init() {
+        self.arrEventRegisted = [];
+        // -- Default system mode --
+        self.arrSupportMode = [
+            NSNotification.Name.UIApplicationDidEnterBackground.rawValue,
+            NSNotification.Name.UIApplicationWillEnterForeground.rawValue,
+            NSNotification.Name.UIApplicationDidFinishLaunching.rawValue,
+            NSNotification.Name.UIApplicationDidBecomeActive.rawValue,
+            NSNotification.Name.UIApplicationWillResignActive.rawValue,
+            NSNotification.Name.UIApplicationDidReceiveMemoryWarning.rawValue,
+            NSNotification.Name.UIApplicationWillTerminate.rawValue,
+            NSNotification.Name.UIApplicationSignificantTimeChange.rawValue]
+    }
+    
+    deinit {
+        DbNotification.remove(self)
+    }
+    
+    func addExtendRunMode(arrMode: Array<String>) -> Void {
+        self.arrSupportMode.append(contentsOf: arrMode)
+        // -- Start AddObserver --
+        self.reAddObserver()
+    }
+    
+    func subscribeEvent(_ event: DbEventObject) -> DbEventRegisterID {
+        // -- Add --
+        self.arrEventRegisted.append(event);
+        // -- Sort --
+        self.arrEventRegisted.sort { (event_1, event_2) -> Bool in
+            return (event_1.eventPriority() >= event_2.eventPriority())
+        }
+        // -- Create eventID --
+        event.eventID = DbEventIDGenerator.getUniqueRegisterID()
+        return event.eventID
+    }
+    
+    func removeEvent(_ event: DbEventObject) -> Void {
+        self.removeEventById(eventId: event.eventID)
+    }
+    
+    func removeEventByGroup(eventGroup: String) -> Void {
+        for obj: DbEventObject in self.arrEventRegisted {
+            if obj.eventGroup() == eventGroup {
+                self.removeEventById(eventId: obj.eventID)
+            }
+        }
+    }
+    
+    func removeEventById(eventId: DbEventRegisterID) -> Void {
+        var removeIndex = -1
+        for i in 0..<self.arrEventRegisted.count {
+            if self.arrEventRegisted[i].eventID == eventId {
+                // -- Cancel event --
+                self.arrEventRegisted[i].cancelEvent()
+                removeIndex = i
+                break
+            }
+        }
+        // -- Remove Event --
+        self.arrEventRegisted.remove(at: removeIndex)
+    }
+    
+    func removeAllEvent() -> Void {
+        for obj: DbEventObject in self.arrEventRegisted {
+            obj.cancelEvent()
+        }
+        self.arrEventRegisted.removeAll()
+    }
+    
+    // MARK: - Private Functions
+    
+    private func getGroupNameByEventObject(_ eventObj: DbEventObject) -> (String) {
+        return eventObj.eventGroup()
+    }
+    
+    private func getPriorityByEventObject(eventObj: DbEventObject) -> Int {
+        return eventObj.eventPriority()
+    }
+    
+    private func reAddObserver() -> Void {
+        // -- Remove all Notify --
+        DbNotification.remove(self)
+        
+        for mode: String in self.arrSupportMode {
+            DbNotification.add(mode, observer: self, selector: #selector(self.processNotificationCenter), object: nil)
+        }
+    }
+    
+    @objc private func processNotificationCenter(notification: DbNotification) -> Void {
+        
+        for obj: DbEventObject in self.arrEventRegisted {
+            if obj.eventRunBackgroundMode().contains(notification.name.rawValue) {
+                obj.startEvent(notification)
+                //print("\(notification.name.rawValue) -------- \(NSStringFromClass(obj))")
+            }
+        }
+    }
+}
+
+
+
