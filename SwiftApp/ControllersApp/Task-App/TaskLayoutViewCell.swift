1366c44833a37917bac6e3d1f1f48d3f6a360899
dylan_bui <duc@propzy.com>
2018-10-01 00:34:13 +0700

    update

---
+++ SwiftApp/ControllersApp/Task-App/TaskLayoutViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  TaskLayoutViewCell.swift
+//  SwiftApp
+//
+//  Created by Dylan Bui on 9/11/18.
+//  Copyright © 2018 Propzy Viet Nam. All rights reserved.
+//
+
+import UIKit
+
+class TaskLayoutViewCell: DbTableViewCell
+{
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
