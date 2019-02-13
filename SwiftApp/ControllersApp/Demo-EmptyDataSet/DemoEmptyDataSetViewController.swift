251b756084bfd0ed3cfc5cad064530c9289fb6d7
Dylan Bui <buivantienduc@yahoo.com>
2019-02-13 18:40:05 +0700

    update

--- SwiftApp/ControllersApp/Demo-EmptyDataSet/DemoEmptyDataSetViewController.swift
+++ SwiftApp/ControllersApp/Demo-EmptyDataSet/DemoEmptyDataSetViewController.swift
@@ -8,23 +8,98 @@
 
 import UIKit
 
-class DemoEmptyDataSetViewController: UIViewController {
-
-    override func viewDidLoad() {
+class DemoEmptyDataSetViewController: UIViewController , DbEmptyStatusController
+{
+    @IBOutlet weak var tableView: UITableView!
+    
+    fileprivate struct CellIdentifier {
+        static let reuseIdentifier = "Cell"
+    }
+    
+    // MARK: - Properties
+    var indexPath = IndexPath()
+    fileprivate var isLoading = false
+    fileprivate var dataCount = 0
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        title = "Loading"
+        
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
+        //        let status = Status(isLoading: true, description: "Lädt…")
+        let status = DbEmptyStatus.simpleLoading
+        
+        show(status: status)
+        
+        self.fetchData()
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func fetchData()
+    {
+        let delayTime = DispatchTime.now() + Double(Int64(4.5 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
+        DispatchQueue.main.asyncAfter(deadline: delayTime) { () -> Void in
+            
+            let done = false
+            
+            if done {
+                // -- Done load --
+                self.dataCount = 7
+                self.tableView.reloadData()
+                
+                print("Hide status !!!")
+                self.hideStatus()
+            } else {
+                self.emptyStauts()
+            }
+        }
     }
-    */
-
+    
+    func emptyStauts()
+    {
+        title = "Empty"
+        
+        let status = DbEmptyStatus(title: "no Data", description: "No data available.💣", actionTitle: "Create ⭐️", image: UIImage(named: "placeholder_instagram")) {
+            self.hideStatus()
+        }
+        
+        self.show(status: status)
+        
+        // -- Auto hide --
+        let delayTime = DispatchTime.now() + Double(Int64(4.5 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
+        DispatchQueue.main.asyncAfter(deadline: delayTime) { () -> Void in
+            self.hideStatus()
+        }
+    }
+    
 }
+
+extension DemoEmptyDataSetViewController: UITableViewDelegate, UITableViewDataSource
+{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return dataCount
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        var cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifier.reuseIdentifier)
+        if cell == nil {
+            cell = UITableViewCell(style: .value1, reuseIdentifier: CellIdentifier.reuseIdentifier)
+        }
+        cell!.textLabel?.text = "Cell"
+        cell!.detailTextLabel?.text = "Click to delete"
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        // let item = self.arr[indexPath.row] as? [String:AnyObject]
+    }
+}
+
+
